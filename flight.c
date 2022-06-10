74048b4666d5724fa3ed8290be32265f058f93ee
Nuno Martinho <201901769@estudantes.ips.pt>
2022-06-10 18:29:40 +0100

    method showAllSample to test.

--- flight.c
+++ flight.c
@@ -14,40 +14,34 @@ void showF(PtList list){
     
 }
 
-
-void print20(PtList list){
-   String s;
-   fgets(s, sizeof(s), stdin);
-   while(strcmp(s, "done")!= 0){
-       printPages(list, page*20);
-       waitForInput(list, &page);
-   }
-}
-
 void showAllPaged(PtList list) {
 
     String command;
     int page = 0;
     int exit = 0;
-
+    Flight flight;
     while (!exit) {
-        clrscr();
-        printf("\n===================================================================================");
-        printf("\n                          ALL FLIGHTS");
-        printf("\n===================================================================================");
+        printFlightsMenu();
+        printf("\nCommands  (NEXT, PREV, EXIT)");
+        
+        /* 
+         * Starts the index in the previous element (page = 0 * 20 = 0 starts in 0 element, page = 1 * 20 starts in the element 20) 
+         * To show only 20 pages, we do: page (0) + 1 * 20 = 20 pages in page 0, page (1) + 1 * 20 = 40 starts in the element 20 to element 40 (more 20 pages).
+         */
         for (int i = page*20; i < (page+1)*20; i++) {
-            listElemPrint(list->elements[i]);
+            listGet(list, i, &flight);
+            listElemPrint(flight);
         }
+        
         if (page > 0)
             printf("\n<------------PREV------------- PAGE [%d] ------------NEXT-------------> ", page);
         else
             printf("\n<------------EXIT------------- PAGE [%d] ------------NEXT-------------> ", page);
         
-        printf("\nCommands  (NEXT, PREV, EXIT)");
         printf("\ncommand> ");
         fgets(command, sizeof(command), stdin);
         command[strlen(command) - 1] = '\0';
-
+        //problema e se chegar na ultima pagina?
         if (equalsStringIgnoreCase(s, "NEXT")){
             page++;
         }
@@ -64,5 +58,20 @@ void showAllPaged(PtList list) {
 }
 
 void showAllSample(PtList list) {
-    
+    printFlightsMenu();
+    int size;
+    listSize(list, &size);
+    Flight flight;
+    for (int i = 0; i < 100; i++) {
+        int number = (rand() % ((size-1) - 0 + 1)) + 0;
+        listGet(list, number, &flight);
+        listElemPrint(flight);
+    }
+}
+
+void printFlightsMenu() {
+        clrscr(); // clear console.
+        printf("\n===================================================================================");
+        printf("\n                          ALL FLIGHTS");
+        printf("\n===================================================================================");
 }
